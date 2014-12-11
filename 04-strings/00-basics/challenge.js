fc7309949796fa338f9033f0d77ff4e8b5729f2a
Steph <stfwong@gmail.com>
2014-12-11 07:54:34 +0000

    string exercises

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,21 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName + ",\n\n" + message + "\n\n" + "Sincerely,\n" + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+  return bigString.substring(startA,endA) + bigString.substring(startB,endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  var index1 = text.indexOf(searchString);
+  var index2 = text.lastIndexOf(searchString);
+  return text.substring(index1 + searchString.length, index2);
 };
