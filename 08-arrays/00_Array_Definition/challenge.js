8a5ddf01a162c3f9171dda24a1496ac9a5cd01f4
Steph <stfwong@gmail.com>
2014-12-16 07:15:25 +0000

    arrays part 1

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,11 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a,b,c,d){
+  return [a,b,c,d];
+};
 
-module.exports.firstAndLast = undefined;
+//var array1 = ['yes', 8, 'cool', 5];
+module.exports.firstAndLast = function(array1){
+  var array1 = ['yes', 8, 'cool', 5];
+  var lastArrayElement = array1.length -1
+  return [array1[0], array1[lastArrayElement];
+  
+};
