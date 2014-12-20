fac58a37dcf2c816b23daa7ec30df0d5f64efd57
Steph <stfwong@gmail.com>
2014-12-20 23:36:37 +0000

    array exercises

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -2,10 +2,13 @@ module.exports.newArray = function(a,b,c,d){
   return [a,b,c,d];
 };
 
-//var array1 = ['yes', 8, 'cool', 5];
-module.exports.firstAndLast = function(array1){
-  var array1 = ['yes', 8, 'cool', 5];
-  var lastArrayElement = array1.length -1
-  return [array1[0], array1[lastArrayElement];
+
+module.exports.firstAndLast = function(array){
+  if(array.length < 3){
+    return "The array is not long enough.";
+  }
+  else{
+    return [array[0], array[array.length-1]];
+  }
   
 };
--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,20 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array){
+  if(array.length < 2){
+    return "The array is not long enough.";
+  }
+  else{
+    array.reverse().unshift(1);
+    return array;
+  }
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array){
+  if(array.length < 2){
+    return array.replace(array[0].replace("+"));
+  }
+  else{
+    return array.toString().replace(/,/g,"+");
+  }
+};
 
 module.exports.arrayQuantityPlusOne = undefined;
