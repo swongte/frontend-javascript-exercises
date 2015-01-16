c7049567ad85f1e5ce236ec3a5608eb30d5ede3d
Steph <stfwong@gmail.com>
2015-01-16 07:56:36 +0000

    edited reverseSortedList

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -10,5 +10,18 @@ module.exports.addItem = function(item, list){
 };
 
 module.exports.reverseSortedList = function(array){
-  return array.sort().reverse();
+  if(array.indexOf(/./g) < 0){
+    return array.sort(function(a,b){
+        if (b < a) {
+            return -1;
+        }
+        if (b > a) {
+            return 1;
+        }
+        // a must be equal to b
+            return 0;});
+    }
+  else{
+    return array.sort(function compareNumbers(a, b) {return b - a;});
+  }
 };
