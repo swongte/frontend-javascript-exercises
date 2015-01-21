43f82ba68769afc656cf70e9fdc0d7531b1a0df1
Steph <stfwong@gmail.com>
2015-01-21 07:24:56 +0000

    Object exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -14,10 +14,30 @@ module.exports.addProperty = function(object, newProp, newValue){
   }
   else{
     object[newProp] = newValue;
+    return object;
   }
   
 };
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(myLetter){
+  
+  return "Hello " + myLetter.recipient + ",\n\n" + myLetter.msg + "\n\nSincerely,\n" + myLetter.sender;
+  
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item,money){
+  itemToBuy = {};
+    itemToBuy.name = item;
+    itemToBuy.value = money;
+    
+    appleProduct = {
+        'MacBook Air': 999,
+        'MacBook Pro': 1299,
+        'Mac Pro': 2499,
+        'Apple Sticker': 1
+    };
+    
+    return appleProduct.hasOwnProperty(itemToBuy.name) && (itemToBuy.name == 'MacBook Air' && itemToBuy.value >= appleProduct['MacBook Air']) || (itemToBuy.name == 'MacBook Pro' && itemToBuy.value >= appleProduct['MacBook Pro']) || (itemToBuy.name == 'Mac Pro' && itemToBuy.value >= appleProduct['Mac Pro']) || (itemToBuy.name == 'Apple Sticker' && itemToBuy.value >= appleProduct['Apple Sticker']);
+  
+  
+};
